ac5c4004ecc4542ae57fdf3d2dd6ce8538ce3384
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2023-05-12 16:35:40 +0700

    Refactor: Pack ffmpeg with the application.

--- Meelody.py
+++ Meelody.py
@@ -1,4 +1,6 @@
 import io
+import os
+import subprocess
 import sys
 from sys import argv, exit
 
@@ -9,21 +11,36 @@ from modules.screens.Application import Application
 from modules.statics.view.Material import Icons
 from modules.widgets.Applications import SingletonApplication
 
-if sys.stderr is None:
-    stream = io.StringIO()
-    sys.stdout = stream
-    sys.stderr = stream
+
+def _initConsole():
+    if sys.stderr is None:
+        stream = io.StringIO()
+        sys.stdout = stream
+        sys.stderr = stream
+
+
+def _run_ffmpeg():
+    basedir = str(os.path.dirname(os.path.abspath(__file__)))
+    startupInfo = subprocess.STARTUPINFO()
+    startupInfo.dwFlags |= subprocess.STARTF_USESHOWWINDOW
+    subprocess.call(f'{basedir}/ffmpeg.exe', startupinfo=startupInfo)
 
 
 def run_application():
+    _initConsole()
+    _run_ffmpeg()
+
     APP_NAME = "Meelody"
+
     app = SingletonApplication(argv, APP_NAME)
     app.setAttribute(Qt.AA_DontCreateNativeWidgetSiblings)
     app.setApplicationName(APP_NAME)
     application = Application()
     app.messageSent.connect(application.receiveMessage)
-    Times.measure(lambda: application.run(), lambda time: print(f"Time to start application: {time}"))
     app.setWindowIcon(Icons.LOGO)
+
+    Times.measure(lambda: application.run(), lambda time: print(f"Time to start application: {time}"))
+
     exit(app.exec_())
 
 
