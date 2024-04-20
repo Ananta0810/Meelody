91ac5f9fe8250d36d18c7855ac7af63a36172484
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2024-04-20 10:33:33 +0700

    Refactor: Use list classname in translators instead of list of string.

--- app/helpers/stylesheets/translators/props_translators/background_translator.py
+++ app/helpers/stylesheets/translators/props_translators/background_translator.py
@@ -1,21 +1,24 @@
 from typing import List
 
 from app.helpers.stylesheets import Color
+from app.helpers.stylesheets.translators.props_translators.class_name import ClassName
 from app.helpers.stylesheets.translators.props_translators.props_translator import PropsTranslator
 from app.helpers.stylesheets.translators.value_translators import ValueTranslators
 
 TRANSPARENT_COLOR = Color(0, 0, 0, 0)
 
 
-def _toProps(name: str) -> Color | None:
-    color = name[name.find('-') + 1:]
+def _toProps(cn: ClassName) -> Color | None:
+    color = cn.value
+    if color is None:
+        raise ValueError(f"Invalid color, please add color to {cn}")
     try:
         if color in ["transparent", "none"]:
             return TRANSPARENT_COLOR
 
         return ValueTranslators.Color.translate([color])
     except Exception:
-        raise ValueError(f"Invalid className: {name}")
+        raise ValueError(f"Invalid className: {cn}")
 
 
 class BackgroundTranslator(PropsTranslator):
@@ -26,6 +29,6 @@ class BackgroundTranslator(PropsTranslator):
     def id(self) -> str:
         return "bg"
 
-    def translate(self, names: List[str]) -> str:
+    def translate(self, names: List[ClassName]) -> str:
         color = _toProps(names[-1])
         return f"background: {'None' if color is None else color.toStylesheet()}"
--- app/helpers/stylesheets/translators/props_translators/border_translator.py
+++ app/helpers/stylesheets/translators/props_translators/border_translator.py
@@ -2,24 +2,24 @@ from typing import List
 
 from app.helpers.base import Dicts
 from app.helpers.stylesheets import Color
+from app.helpers.stylesheets.translators.props_translators.class_name import ClassName
 from app.helpers.stylesheets.translators.props_translators.props_translator import PropsTranslator
-from app.helpers.stylesheets.translators.value_translators.color_translator import ColorTranslator
+from app.helpers.stylesheets.translators.value_translators import ValueTranslators
+
+__STYLES = {"solid", "dotted", "dashed", "double", "hidden"}
 
 
-def _toProps(name: str):
-    parts = name.split("-")
-    if len(parts) == 1:
+def _toProps(cn: ClassName):
+    if cn.value is None:
         return "size", 1
 
-    if parts[1].isdigit():
-        return "size", int(parts[1])
+    if cn.value.isdigit():
+        return "size", int(cn.value)
 
-    if len(parts) == 2:
-        return "style", parts[2]
+    if cn.value in __STYLES:
+        return "style", cn.value
 
-    color = parts[1:]
-    translator = ColorTranslator()
-    return "color", translator.translate(color)
+    return "color", ValueTranslators.Color.translate([cn.value])
 
 
 class BorderTranslator(PropsTranslator):
@@ -30,7 +30,7 @@ class BorderTranslator(PropsTranslator):
     def id(self) -> str:
         return "border"
 
-    def translate(self, names: List[str]) -> str:
+    def translate(self, names: List[ClassName]) -> str:
         dictionary = dict([_toProps(name) for name in names])
 
         size = Dicts.getFrom(dictionary, 'size', otherwise=BorderTranslator.__defaultSize)
