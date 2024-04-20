8b0d679958c89fc426a6b36c8bc8b900db478596
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2024-04-20 16:45:17 +0700

    Feat: Add rounded translator.

--- app/helpers/stylesheets/translators/props_translators/props_translators.py
+++ app/helpers/stylesheets/translators/props_translators/props_translators.py
@@ -2,10 +2,12 @@ from typing import final
 
 from app.helpers.stylesheets.translators.props_translators.background_translator import BackgroundTranslator
 from app.helpers.stylesheets.translators.props_translators.border_translator import BorderTranslator
+from app.helpers.stylesheets.translators.props_translators.rounded_translator import RoundedTranslator
 
 
 @final
 class PropsTranslators:
     Border = BorderTranslator()
     Background = BackgroundTranslator()
-    Translators = [Border, Background]
+    Rounded = RoundedTranslator()
+    Translators = [Border, Background, Rounded]
---
+++ app/helpers/stylesheets/translators/props_translators/rounded_translator.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from PyQt5.QtWidgets import QWidget
+
+from app.helpers.base import Lists
+from app.helpers.stylesheets.translators.props_translators.class_name import ClassName
+from app.helpers.stylesheets.translators.props_translators.props_translator import PropsTranslator
+
+__SIZES = {
+    "sm": 2,
+    "md": 4,
+    "lg": 8,
+    "xl": 16,
+    "2xl": 32
+}
+
+
+def _toProps(cn: ClassName, target: QWidget) -> int | float:
+    if cn.value is None:
+        raise ValueError("Please add value to rounded")
+
+    if cn.value == "full":
+        smallerEdge = min(target.width(), target.height())
+        return smallerEdge / 2
+
+    if cn.value in __SIZES:
+        return __SIZES[cn.value]
+
+    if "/" in cn.value:
+        smallerEdge = min(target.width(), target.height())
+        return smallerEdge * float(cn.value)
+
+    return float(cn.value)
+
+
+class RoundedTranslator(PropsTranslator):
+
+    def id(self) -> str:
+        return "rounded"
+
+    def translate(self, names: List[ClassName], target: QWidget) -> str:
+        cn = Lists.lastOf(names)
+        result = _toProps(cn, target)
+
+        return f"border-radius: {result}px"
