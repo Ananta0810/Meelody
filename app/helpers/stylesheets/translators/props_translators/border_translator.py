dde4e1474d60a26db0124b1fc6535eac5353e9fb
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2024-05-04 15:15:21 +0700

    Feat: Add more directions to border translator.

--- app/helpers/stylesheets/translators/props_translators/border_translator.py
+++ app/helpers/stylesheets/translators/props_translators/border_translator.py
@@ -2,32 +2,53 @@ from typing import Optional
 
 from PyQt5.QtWidgets import QWidget
 
-from app.helpers.base import Dicts, Strings
+from app.helpers.base import Dicts, Strings, Lists
 from app.helpers.stylesheets import Color
 from app.helpers.stylesheets.translators.props_translators.class_name import ClassName
 from app.helpers.stylesheets.translators.props_translators.props_translator import PropsTranslator
 from app.helpers.stylesheets.translators.value_translators import ValueTranslators
 
 _STYLES: set[str] = {"solid", "dotted", "dashed", "double", "hidden"}
-_DIRECTIONS: dict[str, str] = {"l": "left", "t": "top", "r": "right", "b": "bottom"}
-_NULL_DIRECTION_PROPS: list[str] = []
+
 _ALL_DIRECTIONS: str = "all"
 
+_DIRECTIONS: dict[str, list[str]] = {
+    "l": ["left"],
+    "t": ["top"],
+    "r": ["right"],
+    "b": ["bottom"],
+    "x": ["left", "right"],
+    "y": ["top", "bottom"],
+}
 
-def _toProps(cn: ClassName) -> (Optional[str], str, int | str | Color):
+_NULL_DIRECTION_PROPS: list[str] = []
+
+
+def _toProps(cn: ClassName) -> list[(Optional[str], str, int | str | Color)]:
+    # border-t, border-x, border-b,...
     if cn.value in _DIRECTIONS:
-        key, value = _toProps0("1")
-        direction = cn.value
-        return direction, key, value
+        directions = _DIRECTIONS[cn.value]
 
+        result = []
+        for direction in directions:
+            key, value = _toProps0(None)
+            result.append((direction, key, value))
+        return result
+
+    # border-t-2, border-x-solid,....
     if cn.value is not None and "-" in cn.value:
         direction, props = cn.value.split("-", maxsplit=1)
         if direction in _DIRECTIONS:
-            key, value = _toProps0(props)
-            return direction, key, value
+            directions = _DIRECTIONS[direction]
+
+            result = []
+            for direction_ in directions:
+                key, value = _toProps0(props)
+                result.append((direction_, key, value))
+            return result
 
     key, value = _toProps0(cn.value)
-    return _ALL_DIRECTIONS, key, value
+    return [(_ALL_DIRECTIONS, key, value)]
 
 
 def _toProps0(value: Optional[str]) -> (str, int | str):
@@ -55,7 +76,7 @@ class BorderTranslator(PropsTranslator):
         return {"border"}
 
     def translate(self, names: list[ClassName], target: QWidget) -> str:
-        directions = Dicts.group([_toProps(name) for name in names], lambda prop: prop[0])
+        directions = Dicts.group(Lists.flat([_toProps(name) for name in names]), lambda prop: prop[0])
 
         generalProps = {key: value for direction, key, value in directions.get(_ALL_DIRECTIONS, _NULL_DIRECTION_PROPS)}
 
@@ -63,7 +84,10 @@ class BorderTranslator(PropsTranslator):
         color = generalProps.get('color', BorderTranslator.__defaultColor)
         style = generalProps.get('style', BorderTranslator.__defaultStyle)
 
-        props = [f"border: {size}px {style} {color}"]
+        props = []
+
+        if "size" in generalProps:
+            props.append(f"border: {size}px {style} {color}")
 
         for direction, details in directions.items():
             if direction == _ALL_DIRECTIONS:
@@ -75,6 +99,6 @@ class BorderTranslator(PropsTranslator):
             directionColor = directionProps.get('color', color)
             directionStyle = directionProps.get('style', style)
 
-            props.append(f"border-{_DIRECTIONS[direction]}: {directionSize}px {directionStyle} {directionColor}")
+            props.append(f"border-{direction}: {directionSize}px {directionStyle} {directionColor}")
 
         return Strings.joinStyles(props)
