9cfbff1b66c8b20e7a4e05b95190db4f87efd827
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2024-05-08 12:23:09 +0700

    Feat: Add thumbnail buttons.

--- app/views/windows/main_window.py
+++ app/views/windows/main_window.py
@@ -1,9 +1,12 @@
 from PyQt5.QtCore import Qt
+from PyQt5.QtWinExtras import QWinThumbnailToolBar, QWinThumbnailToolButton
 
-from app.common.others import appCenter
+from app.common.others import appCenter, musicPlayer
 from app.components.base import Component
 from app.components.windows.windows import TitleBarWindow
 from app.helpers.base import Strings
+from app.helpers.stylesheets import Colors
+from app.resource.qt import Icons
 from app.views.home import HomeBody
 from app.views.player_bar import MusicPlayerBar
 
@@ -35,8 +38,37 @@ class MainWindow(TitleBarWindow, Component):
         self.addWidget(self._body)
         self.addWidget(self._musicPlayerBar, alignment=Qt.AlignBottom)
 
+        # Prev, Play/Pause, Next
+        self._toolbarPrevBtn = QWinThumbnailToolButton()
+        self._toolbarPrevBtn.setToolTip('Previous')
+        self._toolbarPrevBtn.setIcon(Icons.PREVIOUS.withColor(Colors.PRIMARY))
+
+        self._toolbarPlayBtn = QWinThumbnailToolButton()
+        self._toolbarPlayBtn.setToolTip('Play')
+        self._toolbarPlayBtn.setProperty('status', 0)
+        self._toolbarPlayBtn.setIcon(Icons.PLAY.withColor(Colors.PRIMARY))
+
+        self._toolbarNextBtn = QWinThumbnailToolButton()
+        self._toolbarNextBtn.setToolTip('Next')
+        self._toolbarNextBtn.setIcon(Icons.NEXT.withColor(Colors.PRIMARY))
+
+        self._toolbar = QWinThumbnailToolBar()
+        self._toolbar.addButton(self._toolbarPrevBtn)
+        self._toolbar.addButton(self._toolbarPlayBtn)
+        self._toolbar.addButton(self._toolbarNextBtn)
+
     def _connectSignalSlots(self) -> None:
         self._closeBtn.clicked.connect(lambda: appCenter.exited.emit())
+
+        self._toolbarPrevBtn.clicked.connect(lambda: musicPlayer.playPreviousSong())
+        self._toolbarPlayBtn.clicked.connect(lambda: musicPlayer.pause() if musicPlayer.isPlaying() else musicPlayer.play())
+        self._toolbarNextBtn.clicked.connect(lambda: musicPlayer.playNextSong())
+
+        musicPlayer.played.connect(lambda: self._toolbarPlayBtn.setToolTip("Pause"))
+        musicPlayer.played.connect(lambda: self._toolbarPlayBtn.setIcon(Icons.PAUSE.withColor(Colors.PRIMARY)))
+
+        musicPlayer.paused.connect(lambda: self._toolbarPlayBtn.setToolTip("Play"))
+        musicPlayer.paused.connect(lambda: self._toolbarPlayBtn.setIcon(Icons.PLAY.withColor(Colors.PRIMARY)))
 
     def applyLightMode(self) -> None:
         super().applyLightMode()
@@ -47,3 +79,4 @@ class MainWindow(TitleBarWindow, Component):
     def show(self) -> None:
         super().show()
         self.moveToCenter()
+        self._toolbar.setWindow(self.windowHandle())
