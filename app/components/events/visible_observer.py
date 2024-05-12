9073a6e44370fa52e1017807d966d39f619ee11f
Ananta0810 <mydreamistobecomegooddev@gmail.com>
2024-05-12 12:39:09 +0700

    Fix: Fix songs menu refresh after change settings.

--- app/components/events/visible_observer.py
+++ app/components/events/visible_observer.py
@@ -6,11 +6,14 @@ from app.helpers.qt import Widgets
 class VisibleObserver(QObject):
     visible = pyqtSignal(bool)
 
-    def __init__(self, widget: QObject):
+    def __init__(self, widget: QObject, oneTime: bool = True):
         super().__init__(widget)
         self._widget = widget
         self._isVisible = False
         self._widget.installEventFilter(self)
+
+        if oneTime:
+            self.visible.connect(lambda visible: self.deleteLater() if visible else None)
 
     def eventFilter(self, obj: QObject, event: QEvent) -> bool:
         if obj is not self._widget:
